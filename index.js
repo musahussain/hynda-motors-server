960a2812e608e694a9e509806d6f698519bca122
musahussain <mdmuslimhussain1@gmail.com>
2022-05-22 17:45:10 +0600

    added express mongo cors jwt

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("running server");
+const express = require('express')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server Running');
+})
+
+app.listen(port, () => {
+  console.log(`Running Hynda Motors Server on port ${port}`);
+})
